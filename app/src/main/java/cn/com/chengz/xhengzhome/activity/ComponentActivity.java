f51f2d2f919b06c7c65a649912c5ee3b5726d3fe
chengz <522035487@qq.com>
2016-04-26 18:10:15 +0800

    Bottom sheet and textview copy menu test

---
+++ app/src/main/java/cn/com/chengz/xhengzhome/activity/ComponentActivity.java
@@ -0,0 +1,15 @@
+package cn.com.chengz.xhengzhome.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import cn.com.chengz.xhengzhome.R;
+
+public class ComponentActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_component);
+    }
+}
